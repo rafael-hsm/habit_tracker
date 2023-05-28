a1743c1947e5f7e3e3189be2d28806bdba73abb8
rafael.meireles.prestserv <rafael.meireles.prestserv@petrobras.com.br>
2023-05-28 16:33:59 -0300

    update until date_navigation

---
+++ app.py
@@ -0,0 +1,46 @@
+import datetime
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+habits = ["Test habit", "Test habit 2"]
+
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    return {"date_range": date_range}
+
+
+@app.route("/fizzbuzz")
+def todo():
+    return render_template("fizzbuzz.html")
+
+@app.route("/")
+def index():
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+        
+    return render_template(
+        "index.html",
+        title="Habit Tracker - Home",
+        habits=habits,
+        selected_date=selected_date,
+        )
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_habit():
+    if request.method == "POST":
+        habits.append(request.form.get("habit"))
+    
+    return render_template(
+        "add_habit.html",
+        title="Habit Tracker - Add Habit",
+        selected_date=datetime.date.today(),
+        )
