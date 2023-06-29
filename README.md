6f7e313b14b4747a189bf5620d8d4187d2ff2560
Rafael Meireles <rafameireles2011@gmail.com>
2023-06-29 12:51:00 -0300

    atualização e implementação de novos features

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# habit_tracker
-Habit Tracker - Developer during the course: Flask Bootcamp
+# Habit Tracker
+
+Access the system: https://rafael-habit-tracker.onrender.com
+
+Solution developed during the course: [Web Developer Bootcamp with Flask and Python in 2023](https://www.udemy.com/course/web-developer-bootcamp-flask-python/) and implemented some tweaks and features like deleting a task.
--- routes.py
+++ routes.py
@@ -21,21 +21,27 @@ def todo():
 
 def today_at_midnight():
     today = datetime.datetime.today()
-    return datetime.datetime(today.year, today.month, today.day)
+    return today.replace(hour=0, minute=0, second=0, microsecond=0)
 
 
 @pages.route("/")
 def index():
     date_str = request.args.get("date")
     if date_str:
-        selected_date = datetime.datetime.fromisoformat(date_str)
+        selected_date = datetime.datetime.fromisoformat(date_str).date()
     else:
-        selected_date = datetime.datetime.today()
+        selected_date = datetime.datetime.today().date()
+
+    start_of_day = datetime.datetime.combine(selected_date, datetime.time.min)
+    end_of_day = datetime.datetime.combine(selected_date, datetime.time.max)
     
-    habits_on_date = current_app.db.habits.find({"added": {"$lte": selected_date}})
+    habits_on_date = current_app.db.habits.find({
+        "added": {"$gte": start_of_day, "$lte": end_of_day}
+    })
+    
     completions = [
         habit["habit"]
-        for habit in current_app.db.completions.find({"date": selected_date})
+        for habit in current_app.db.completions.find({"date": start_of_day.strftime("%Y-%m-%d")})
     ]
     
     return render_template(
@@ -44,30 +50,59 @@ def index():
         selected_date=selected_date,
         completions=completions,
         title="Habit Tracker - Home",
-        )
+    )
+
+
 
 
 @pages.route("/add", methods=["GET", "POST"])
 def add_habit():
-    today = today_at_midnight()
-    
     if request.method == "POST":
-       current_app.db.habits.insert_one(
-           {"_id": uuid.uuid4().hex, "added": today, "name": request.form.get("habit")}
-       )
-    
+        habit_name = request.form.get("habit")
+        selected_date_str = request.args.get("date")
+
+        if selected_date_str:
+            selected_date = datetime.datetime.strptime(selected_date_str, "%Y-%m-%d").date()
+        else:
+            selected_date = datetime.datetime.today().date()
+
+        current_app.db.habits.insert_one(
+            {"_id": uuid.uuid4().hex, "added": datetime.datetime.combine(selected_date, datetime.datetime.min.time()), "name": habit_name}
+        )
+
+
+        return redirect(url_for(".index", date=selected_date_str))
+
+    today = today_at_midnight()
+
     return render_template(
         "add_habit.html",
         title="Habit Tracker - Add Habit",
         selected_date=today,
-        )
-    
-    
+    )
+
+       
 @pages.route("/complete", methods=["POST"])
 def complete():
     date_string = request.form.get("date")
-    habit = request.form.get("habitId")
-    date = datetime.datetime.fromisoformat(date_string)
-    current_app.db.completions.insert_one({"date":date, "habit": habit}) 
+    habit_id = request.form.get("habitId")
+    date = datetime.datetime.fromisoformat(date_string).date()
     
+    # Converter a data para uma representação de string
+    date_str = date.strftime("%Y-%m-%d")
+    
+    current_app.db.completions.insert_one({"date": date_str, "habit": habit_id})
+
+    # Atualize a tarefa como concluída no banco de dados
+    current_app.db.habits.update_one(
+        {"_id": habit_id},
+        {"$set": {"completed": True}}
+    )
+
     return redirect(url_for(".index", date=date_string))
+
+
+@pages.route("/delete/<habitId>", methods=['POST'])
+def delete_habit(habitId):
+    current_app.db.habits.delete_one({'_id': habitId})
+    return redirect(url_for('.index'))
